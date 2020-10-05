b81babac28601ffaff4ad68c10fc79c99ab3a76e
Gomding <backchida@gmail.com>
2020-10-05 21:48:43 +0900

    Get 요청에 @JsonFormat 대신 @DateTimeFormat 사용

--- src/main/java/com/test/parse/localdate/dto/GetModel.java
+++ src/main/java/com/test/parse/localdate/dto/GetModel.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -17,7 +18,10 @@ public class GetModel {
     private String name;
 
     // GET 요청에 JsonFormat 어노테이션을 사용함
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
+    // @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
+
+    // DateTimeFormat은 Spring에서 지원하는 어노테이션 LocalDate 나 LocalDateTime 과 같은 날짜관련 타입의 직렬화를 지원함
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime requestDateTime;
 
 }
