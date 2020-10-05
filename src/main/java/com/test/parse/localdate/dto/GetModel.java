2f958e68e7b03119efa02b3dc159b6b3b33a7657
Gomding <backchida@gmail.com>
2020-10-05 21:39:20 +0900

    Get요청에 LocalDateTome을 @JsonFormat 으로 직렬화 시도(실패)

--- src/main/java/com/test/parse/localdate/dto/GetModel.java
+++ src/main/java/com/test/parse/localdate/dto/GetModel.java
@@ -1,5 +1,6 @@
 package com.test.parse.localdate.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,6 +15,9 @@ import java.time.LocalDateTime;
 public class GetModel {
 
     private String name;
+
+    // GET 요청에 JsonFormat 어노테이션을 사용함
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime requestDateTime;
 
 }
